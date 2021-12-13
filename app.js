f641e9c1c326f67815e090d9f42f98116b20e5b2
Lyz03 <lizoe@netcourrier.com>
2021-12-13 11:17:10 +0100

    basic js

---
+++ app.js
@@ -0,0 +1,97 @@
+const resetButtons = document.querySelectorAll('.reset');
+const resultP = document.querySelector('.result p');
+
+//expenses
+const firstPart = document.querySelectorAll('.first_part input');
+const secondPart = document.querySelectorAll('.second_part > input');
+const newExpense = document.querySelector('input[type=submit]');
+const submitExpenses = document.getElementById('expense_button');
+
+//saving
+const saving = document.getElementById('saving');
+const submitSaving = document.getElementById('saving_button');
+
+//receipts
+const receipts = document.querySelectorAll('.receipts input[type=number]');
+const newReceipts = document.querySelector('.receipts input[type=submit]');
+const submitReceipts = document.getElementById('receipts_button');
+
+let a = 0
+let totalExpenses = 0;
+let totalSaving = 0;
+let totalReceipts = 0;
+let finalTotal = 0;
+
+/**
+ * Is there nothing in the input
+ * @param value
+ */
+function isZero(value) {
+    if (value.value === '')
+        value.value = 0;
+}
+
+// add every input value of the first part
+function totalFirstPart() {
+    firstPart.forEach(value => {
+        isZero(value);
+        totalExpenses += parseInt(value.value);
+    });
+}
+
+// add every input value of the second part (checking if it's not monthly)
+function totalSecondPart() {
+    secondPart.forEach(value => {
+        isZero(value);
+
+        if (value.labels[0].innerText.includes('hebdomadaire'))
+            totalExpenses += parseInt(value.value) * 4;
+        else if (value.labels[0].innerText.includes('annuel'))
+            totalExpenses += Math.round(parseInt(value.value) / 12);
+        else
+            totalExpenses += parseInt(value.value);
+    })
+}
+
+// add every input value of the receipts part
+function totalReceiptsF() {
+    receipts.forEach(value => {
+        isZero(value);
+        totalReceipts += parseInt(value.value);
+    });
+}
+
+// add every total and print it in result
+function printTotal() {
+    finalTotal = totalReceipts - (totalExpenses + totalSaving)
+    resultP.innerHTML = finalTotal.toString();
+}
+
+
+submitExpenses.addEventListener("click", () => {
+    totalExpenses = 0;
+    totalFirstPart();
+    totalSecondPart();
+});
+
+submitSaving.addEventListener("click", () => {
+    if (saving.value === '')
+        saving.value = 0;
+
+    totalSaving = parseInt(saving.value);
+});
+
+submitReceipts.addEventListener("click", () => {
+    totalReceipts = 0
+    totalReceiptsF();
+});
+
+// reset buttons
+resetButtons[0].addEventListener("click", () => {
+    firstPart.forEach(value => value.value = '');
+    secondPart.forEach(value => value.value = '');
+});
+
+resetButtons[1].addEventListener("click", () => saving.value = '');
+
+resetButtons[2].addEventListener("click", () => receipts.forEach(value => value.value = ''));
